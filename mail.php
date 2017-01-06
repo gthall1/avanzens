8861aa18ce2a47873640a9bb87f286e6725af154
Griff Hall <griffhall1@gmail.com>
2017-01-06 10:58:39 -0600

    added form functionality

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = $_POST['name'];
+$country = $_POST['country'];
+$code = $_POST['code'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$company = $_POST['company'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Country: $country \n Code: $code \n Phone: $phone \n Email: $email \n Company: $company \n Message: $message";
+$recipient = "griff@rockbottomcreativeagency.com";
+$subject = "Website Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+
+header("Location: thanks.html");
+
+?>
+
